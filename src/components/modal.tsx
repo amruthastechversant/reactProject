a81e7e176cd8083161ad1f22d593f6d5483c9375
amruthastechversant <amrutha.s@techversantinfotech.com>
2025-12-01 11:56:24 +0530

    ToDo with drag and drop,crud,status features

--- src/components/modal.tsx
+++ src/components/modal.tsx
@@ -4,21 +4,18 @@ import "../App.css";
 
 interface  modalprops {
     isOpen : boolean;
-    title : string;
     message : string;
     onConfirm : () => void;
     onCancel : () => void;
 }
 
-const Modal:React.FC<modalprops> = ({ isOpen, title, message, onConfirm, onCancel }) => {
+const Modal:React.FC<modalprops> = ({ isOpen,message, onConfirm, onCancel }) => {
   if (!isOpen) return null;
 
   return (
     <div className="modal-overlay">
       <div className="modal-box">
-        <h2>{title}</h2>
         <p className="message">{message}</p>
-
         <div className="modal-buttons">
           <button className="btn cancel" onClick={onCancel}>
             Cancel
--- src/pages/ToDO.tsx
+++ src/pages/ToDO.tsx
@@ -1,13 +1,21 @@
 import { useState } from 'react'
 import Modal from '../components/modal';
+import { CheckCircle } from 'react-bootstrap-icons';
+import { FaEdit, FaTrash } from 'react-icons/fa';
+import { DragDropContext, Draggable, Droppable } from '@hello-pangea/dnd';
 
 function ToDo(){
+    interface Task{
+        text:string,
+        completed:boolean
+    }
     const [task,setTask] = useState<string>("");
-    const [tasks,setTasks] = useState<string[]>([]);
+    const [tasks,setTasks] = useState<Task[]>([]);
     const [editIndex,seteditIndex] = useState<number|null>(null);
     const [error,setError] = useState<string>("");
     const [openModal,setopenModal] = useState(false);
     const [deleteIndex,setdeleteIndex] = useState<number|null>(null);
+    // const [isDisabled,setIsDisabled] = useState(false);
     const addorUpdateTask = ()=>{
         if(task.trim()===""){
             setError("Enter Tasks");
@@ -15,16 +23,15 @@ function ToDo(){
         }
         
         if(editIndex!=null){
-        const updatedTask = [...tasks];
-        updatedTask[editIndex] = task;
-        setTasks(updatedTask);
+            const updatedTask = [...tasks];
+            updatedTask[editIndex].text = task;
+            setTasks(updatedTask);
 
-
-        seteditIndex(null);
-        setTask("");
+            seteditIndex(null);
+            setTask("");
         }else{
-        setTasks([task,...tasks]);
-        setTask("");
+            setTasks([{text:task,completed:false},...tasks]);
+            setTask("");
         }
     }
 
@@ -32,49 +39,83 @@ function ToDo(){
         if(deleteIndex != null){
             setTasks(tasks.filter((_, i) => i !== deleteIndex));
         }
+
+        if(editIndex == deleteIndex){
+            seteditIndex(null);
+            setTask("");
+        }
         setopenModal(false);
 
     };
 
     const editTask = (index:number) => {
-        setTask(tasks[index]);
+        setTask(tasks[index].text);
         seteditIndex(index);
     }
 
-    // const handleConfirm = () =>{
-    //     setopenModal(false);
-    //     alert("confirmed")
-    // }
-
-    // const handleCancel = () =>{
-    //     setopenModal(false);
-    // }
     const handleDelete = (index:number) =>{
         setdeleteIndex(index);
         setopenModal(true);
     }
+
+    const handleDragEnd = (result:any) =>{
+        const updatedTask = [...tasks]
+        const [movedItem] = updatedTask.splice(result.source.index,1)
+        updatedTask.splice(result.destination.index,0,movedItem)
+        setTasks(updatedTask);
+    }
+
+    const handleCompleteTask = (index:number) =>{
+        setTasks(prev =>
+            prev.map((task,i) =>
+                i==index ? {...task,completed:true}:task
+            )
+            
+        )          
+    }
+
+    const handleUndoTask = (index:number) =>{
+        const updatedTask = [...tasks]
+        updatedTask[index].completed = false;
+        setTasks(updatedTask);
+    }
+    
     return (
         <>
             <div className='todo-container'>
                 <h1>To Do List</h1>
-                <div style={{display: 'flex', alignItems: 'center' }}>
+                <div className='input-div'>
                     <input type='text' className="todo-input" value={task} placeholder='enter task' onChange={(e)=>setTask(e.target.value)}/>
                     <button onClick={addorUpdateTask} className='add-btn'>{editIndex!==null?"UPDATE":"ADD"}</button>
                 </div>
                 {error && <p className="error">{error}</p>}
-                <ul className='todo-list'>
-                {tasks.map((item,index) =>(
-                    <li key={index} className='todo-item'>
-                        {item}
-                        <button onClick={()=>editTask(index)} className='edit-btn'>EDIT</button>
-                        <button onClick={()=>handleDelete(index)} className='delete-btn'>DELETE</button>
-                    </li>
-                ))}
-                </ul>
+                <DragDropContext onDragEnd={handleDragEnd}>
+                    <Droppable droppableId='tasks'>
+                        {(provided) => (
+                            
+                        <ul className='todo-list' {...provided.droppableProps} ref={provided.innerRef}>
+                        {tasks.map((item,index) =>(
+                            <Draggable key={index} draggableId={`${item}-${index}`}index={index}>
+                                {(provided) => (
+                                <li className='todo-item' ref={provided.innerRef} {...provided.dragHandleProps} {...provided.draggableProps}>
+                                    {item.text}
+                                    <FaEdit onClick={()=>editTask(index)} className='edit-btn'/>
+                                    <FaTrash onClick={()=>handleDelete(index)} className='delete-btn'/>
+                                    <button onClick={()=>handleCompleteTask(index)} className='status-btn' disabled ={item.completed}>{item.completed?"completed":"Mark as completed"}</button>               
+                                    { item.completed &&<CheckCircle size={20} color="green" />}
+                                    {item.completed && <button onClick={()=>handleUndoTask(index)} className='undo-btn'>Undo</button>}
+                                </li>
+                                )}
+                            </Draggable>
+                        ))}
+                            {provided.placeholder}
+                        </ul>
+                    )}
+                    </Droppable>
+                </DragDropContext>
             </div>
             <Modal
                 isOpen = {openModal}
-                title = "Delete Task"
                 message = "Are you sure want to delete this Task?"
                 onConfirm = {deleteTask}
                onCancel={() => setopenModal(false)}
